8dc5344c5a35cb5e50d6e49fcbc67b9ebac552e4
Max Kuo <maxkuo712@gmail.com>
2022-03-13 02:39:23 +0800

    Ver2: Change to OOP / upload .jar

---
+++ src/tw/Max/RunCode/guessNumberGame.java
@@ -0,0 +1,10 @@
+package tw.Max.RunCode;
+
+import tw.Max.Class.GuessNumberGameClass;
+
+public class guessNumberGame {
+
+    public static void main(String[] args) {
+    	new GuessNumberGameClass();
+    }
+}
